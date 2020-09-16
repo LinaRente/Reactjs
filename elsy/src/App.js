71b1b98324feead8811582e18c5db24a60dc210e
LinaRente <linarente@gmail.com>
2020-09-16 16:51:06 +0200

    elsy

---
+++ elsy/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+const MIN_TEMPERATURE = -20
+const MAX_TEMPERATURE = 40
+const MIN_HEART = 80
+const MAX_HEART = 180
+const MIN_STEPS = 0
+const MAX_STEPS = 50000
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+          <h1>Bonjour!</h1>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ first-test/src/components/Content.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Content extends Component { 
+    render() { 
+        return (
+            <div>
+                <p>{this.props.react}</p>
+            </div>
+        );
+    }
+}
+export default Content;
