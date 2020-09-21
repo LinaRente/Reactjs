f071178142672f24f2fe09d73d65e211771687b2
LinaRente <linarente@gmail.com>
2020-09-21 23:07:17 +0200

    counter

---
+++ react_counter/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Counter from './components/Counter'
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.incrementCount = this.incrementCount.bind(this);
+        this.decrementCount = this.decrementCount.bind(this);
+    }
+    state = {
+
+        count: 1
+    }
+
+    incrementCount() {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+    decrementCount() {
+        this.setState({
+            count: Math.max(this.state.count - 1,0)
+        })
+    }
+    render() {
+        return (
+            <div className='App'>
+                <Counter count={this.state.count}
+                    incrementFn={this.incrementCount}
+                    decrementFn={this.decrementCount}>
+                </Counter>
+            </div>
+        )
+    }
+}
+export default App;
---
+++ react_counter/src/components/Counter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Counter extends React.Component {
+    render() {
+        return (
+            <div style={{ textAlign: "center" }}>
+                <h1 style={{ fontSize: '100px' }}>Counter</h1>
+                <h2 style={{ fontSize: '50px' }}>{this.props.count}</h2>
+                <button onClick={this.props.incrementFn} style={{ border: '3px solid', width: '200px', height: '50px', background: 'green' }}> + </button>
+                <button onClick={this.props.decrementFn} style={{ border: '3px solid', width: '200px', height: '50px', background: 'red' }}>  -  </button>
+            </div>
+        )
+    }
+}
+export default Counter
