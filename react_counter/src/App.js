79f2cdbe2d59c675de1f790b787d80c68e886671
LinaRente <linarente@gmail.com>
2020-09-22 12:02:37 +0200

    modif-counter

--- react_counter/src/App.js
+++ react_counter/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Counter from './components/Counter'
+import './App.css'
 
 class App extends React.Component {
 
--- react_counter/src/components/Counter.js
+++ react_counter/src/components/Counter.js
@@ -3,11 +3,11 @@ import React from 'react';
 class Counter extends React.Component {
     render() {
         return (
-            <div style={{ textAlign: "center" }}>
-                <h1 style={{ fontSize: '100px' }}>Counter</h1>
-                <h2 style={{ fontSize: '50px' }}>{this.props.count}</h2>
-                <button onClick={this.props.incrementFn} style={{ border: '3px solid', width: '200px', height: '50px', background: 'green', fontSize: '30px'}}> + </button>
-                <button onClick={this.props.decrementFn} style={{ border: '3px solid', width: '200px', height: '50px', background: 'red', fontSize: '30px' }}>  -  </button>
+            <div>
+                <h1>Counter</h1>
+                <h2>{this.props.count}</h2>
+                <button className='btn btn-green' onClick={this.props.incrementFn}> + </button>
+                <button className='btn btn-red' onClick={this.props.decrementFn}>  -  </button>
             </div>
         )
     }
