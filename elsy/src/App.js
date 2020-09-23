08b52c7e04c2d4d5672cd71a8260782fcb0a5f6b
LinaRente <linarente@gmail.com>
2020-09-23 23:23:10 +0200

    ...

--- elsy/src/App.js
+++ elsy/src/App.js
@@ -19,13 +19,13 @@ class App extends React.Component {
     this.onStepsChange = this.onStepsChange.bind(this)
     this.onTemperatureChange = this.onTemperatureChange.bind(this)
     this.calculateWater = this.calculateWater.bind(this)
-  }
 
-  state = {
-    water: 0,
-    heart: 120,
-    temperature: -10,
-    steps: 3000
+    this.state = {
+      water: 0,
+      heart: 120,
+      temperature: -10,
+      steps: 3000
+    }
   }
 
   onHeartChange(val) {
@@ -40,17 +40,80 @@ class App extends React.Component {
     })
   }
 
-  onTemperatureChange(val) {
+  // onTemperatureChange(val) {
+  //   this.setState({
+  //     temperature: val
+  //   })
+  // }
+
+  calculateWater(val, element) {
+    if (element === 'temperature') {
+      console.log('calculette water temperature')
+      if (element === 'temperature') {
+        console.log('App#calculateWater#Temperature');
+        //Si le slider Temperature est au dessus de 20°C
+        if (val > 20) {
+          if (this.state.heart > 120) {
+            if (this.state.steps > 10000) {
+              this.setState({
+                water: 1.5 + ((val - 20) * 0.02) + ((this.state.heart - 120) * 0.0008) + ((this.state.steps - 10000) * 0.00002)
+              })
+            } else {
+              this.setState({
+                water: 1.5 + ((val - 20) * 0.02) + ((this.state.heart - 120) * 0.0008)
+              })
+            }
+          } else {
+            if (this.state.steps > 10000) {
+              this.setState({
+                water: 1.5 + ((val - 20) * 0.02) + ((this.state.steps - 10000) * 0.00002)
+              })
+            } else {
+              this.setState({
+                water: 1.5 + (val - 20) * 0.02
+              })
+            }
+          }
+        } else {
+          //Si le slider est en dessous de 20 °C
+          if (this.state.heart > 120) {
+            if (this.state.steps > 10000) {
+              this.setState({
+                water: 1.5 + ((this.state.heart - 120) * 0.0008) + ((this.state.steps - 10000) * 0.00002)
+              })
+            } else {
+              this.setState({
+                water: 1.5 + ((this.state.heart - 120) * 0.0008)
+              })
+            }
+          } else {
+            if (this.state.steps > 10000) {
+              this.setState({
+                water: 1.5 + ((this.state.steps - 10000) * 0.00002)
+              })
+            } else {
+              this.setState({
+                water: 1.5
+              })
+            }
+          }
+        }
+      } //if (element === 'temperature')
+    } if (element === 'steps') {
+      console.log('calculette water step')
+    }
+    if (element === 'heart') {
+      console.log('calculette water heart')
+    }
+  }
+
+  onTemperatureChange(val) { // methode pour changer la valeur de la temperature
+    this.calculateWater(val, 'temperature')
     this.setState({
-      temperature: val
+      temperature: val,
     })
   }
 
-  calculateWater(val) {
-    this.setState({
-      water: val
-    })
-  }
 
   render() {
     return (
@@ -78,8 +141,6 @@ class App extends React.Component {
           max={MAX_TEMPERATURE}
           min={MIN_TEMPERATURE}>
         </Temperature>
-
-
       </div>
     );
   }
